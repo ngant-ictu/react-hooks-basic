d84dcab52f49eb4df014154e1d35b3ae8993ea1c
nga <nga@vietswiss.com>
2021-12-22 08:06:49 +0700

    todo list

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import React, { useState } from 'react';
 import styles from '../styles/Home.module.css'
-import ColorBox from '../components/ColorBox/Index'
+import TodoList from '../components/TodoList'
 
 export default function Home() {
+  const [ todoList, setTodoList ] = useState([
+    { id: 0, title: 'Co gang len' },
+    { id: 1, title: 'Lam sao de tu tin nhat khi di lam' },
+    { id: 2, title: 'Lam sao de dong nghiep than thien voi minh hon' }
+  ])
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+
+    const Index = todoList.findIndex(x => x.id === todo.id);
+    if(Index < 0) return;
+    
+    const newTodoList = [...todoList];
+    newTodoList.splice(Index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
-    <ColorBox />
+
+    <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+    // <ColorBox />
   )
 }
